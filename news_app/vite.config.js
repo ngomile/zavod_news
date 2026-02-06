da13b69dcb2f4bf6890fc448755c15adecf84b51
Noel Gomile <ngomile932@gmail.com>
2026-02-07 00:57:20 +0200

    Initial commit

---
+++ news_app/vite.config.js
@@ -0,0 +1,23 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig(() => ({
+  plugins: [react()],
+  build: {
+    assetsDir: "static",
+    rollupOptions: {
+      input: {
+        app: "./index.html",
+      },
+    },
+    outDir: "./templates/news_app",
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
