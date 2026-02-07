c172262a4a7997ac38ad5500e450d5135214b927
Noel Gomile <ngomile932@gmail.com>
2026-02-07 11:42:20 +0200

    Add news admin

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Article,
+    ArticleImage,
+    Tag,
+    Reaction,
+    Comment
+)
+
+
+class ArticleImageInline(admin.TabularInline):
+    model = ArticleImage
+
+
+class ReactionsInline(admin.TabularInline):
+    model = Reaction
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        ArticleImageInline,
+        ReactionsInline,
+    ]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Reaction)
+class ReactionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
